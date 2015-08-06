24396ef31b70f1d43c031daedfb9cbf41f16ed2e
Adam Ouellette <adamouellette57@gmail.com>
2015-08-06 15:31:48 -0400

    fixed sutomation

--- Programability/Pi/automation.sh
+++ Programability/Pi/automation.sh
@@ -5,6 +5,6 @@ cd home/pi/DestinationMetricsLLC/Programability/Pi
 git fetch origin
 reslog=$(git log HEAD origin/master --oneline)
 
-if [[ "${reslog}" != "" ]] ; then
+if [ "${reslog}" != "" ] ; then
   git merge origin/master # completing the pull
 fi
